dd61f9efcb6e0f8b049c71eefa57071854d85461
ObedDM <pacocobot2@gmail.com>
2025-02-07 15:35:18 -0600

    Added simple regex matching

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,21 @@
-fn main() {
-    //Regex identifier
+use regex::Regex;
 
-    println!("Hello, world!");
-    print!("AYUUUU");
+fn main() {
+     
+    let re: &str = "[A-Za-z]\\.[a-z$!?_]*";
+    println!("{}",re);
+
+    let var = Regex::new(re); //Object class Regex
+    let a= var.unwrap(); //unwrap method
+
+    let ejemplo= "s.text";
+
+    if a.is_match(ejemplo)
+    {
+        println!("correcto ayuuuu");
+    }
+    else
+    {
+        println!("NO, INCORRECTO");
+    }
 }

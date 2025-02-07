6f6b0cac6f5c55a374115029e171a051907f9c26
ObedDM <pacocobot2@gmail.com>
2025-02-07 15:45:30 -0600

    added console input

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::io;
 
 fn main() {
      
@@ -8,9 +9,12 @@ fn main() {
     let var = Regex::new(re); //Object class Regex
     let a= var.unwrap(); //unwrap method
 
-    let ejemplo= "s.text";
+    let mut entrada = String::new();
+    io::stdin().read_line(&mut entrada).unwrap();
+    println!("{}",entrada);
+    let entrada = entrada.trim();
 
-    if a.is_match(ejemplo)
+    if a.is_match(entrada)
     {
         println!("correcto ayuuuu");
     }

b9b20a7a0c859f2c2cf5e46ba4c8274ed799bb13
ObedDM <pacocobot2@gmail.com>
2025-02-07 12:38:46 -0600

    main branch

---
+++ main/target/debug/deps/main-479d1617bf5520fc.d
@@ -0,0 +1,5 @@
+/home/ayu/Documents/VSCode Projects/Rust-Compiler/main/target/debug/deps/main-479d1617bf5520fc: src/main.rs
+
+/home/ayu/Documents/VSCode Projects/Rust-Compiler/main/target/debug/deps/main-479d1617bf5520fc.d: src/main.rs
+
+src/main.rs:
---
+++ main/target/debug/main.d
@@ -0,0 +1,1 @@
+/home/ayu/Documents/VSCode\ Projects/Rust-Compiler/main/target/debug/main: /home/ayu/Documents/VSCode\ Projects/Rust-Compiler/main/src/main.rs

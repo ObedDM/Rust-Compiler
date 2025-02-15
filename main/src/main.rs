3561f517c58217d3be455f87d99388f9ebae0b54
ObedDM <pacocobot2@gmail.com>
2025-02-15 17:11:03 -0600

    Added categorize_line from fernanda

--- main/src/main.rs
+++ main/src/main.rs
@@ -95,6 +95,7 @@ fn main() {
     tokens.insert("AS", vec!['=']);
     tokens.insert("DEL", vec![';', ',', ]);
     tokens.insert("SPACE", vec![' ']);
+    tokens.insert("AOP", vec!['+', '-', '*', '/', '%']); //Arithmetic operators
 
     //Sample of a line to be processed
     let test_string: &str = "!i = i.num + 444;";
@@ -160,3 +161,45 @@ fn main() {
 
 }
 
+
+
+
+
+fn categorize_line(line:&str) -> i8 {
+
+    let category_map: HashMap<&str, i8> = HashMap::from([
+        ("dec", 0), //line cat = declaring var
+        ("aop", 1), //line cat = arithmetic operation
+        ("asg", 2),  //line cat = asign into var
+        ("dec-aop", 3), //line cat = declaring & arithmetic operation
+        ("dec-asg", 4), //line cat = declaring & asign into var
+        ]);
+
+    if line.starts_with('!') { // is it dec?
+        if  line.contains('=') { // is it dec only?
+            if line.contains(['+', '-', '*', '/', '%']) { // is it dec + aop
+                return category_map["dec-aop"];
+            }
+
+            else {
+                return category_map["dec-asg"];
+            }
+        }
+        
+        else {
+            return category_map["dec"];
+        }   
+    }
+
+    else {
+        if line.contains(['+', '-', '*', '/', '%']) {
+            return category_map["aop"];
+            
+            }
+
+        else {   
+            return category_map["asg"];
+
+        }
+    }
+}

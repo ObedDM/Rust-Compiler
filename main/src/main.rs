de53c69cb367614ddd66fecb7133d1bb66d89ca1
ObedDM <pacocobot2@gmail.com>
2025-02-14 21:02:57 -0600

    Inserts each lexeme into a set of unique lexemes in order of appearance

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, vec};
+use std::{collections::HashMap, string, vec};
 use indexmap::IndexSet;
 use regex::Regex;
 
@@ -46,7 +46,7 @@ fn is_end_of_lex(c: char, token_map: &HashMap<&str, Vec<char>>) -> (bool, String
     for (token_type, lex_list) in token_map {
 
         //Checks for possible "end-lexeme" indicating characters; add to the condition as needed
-        if (*token_type == "DEL") || (*token_type == "AS") || (*token_type == "SEP") {
+        if (*token_type == "DEL") || (*token_type == "AS") || (*token_type == "SPACE") {
             
             //Checks if current character represents the end of a lexeme
             if lex_list.contains(&c) {
@@ -94,35 +94,49 @@ fn main() {
     let mut tokens: HashMap<&str, Vec<char>> = HashMap::new();
     tokens.insert("AS", vec!['=']);
     tokens.insert("DEL", vec![';', ',', ]);
-    tokens.insert("SEP", vec![' ']);
+    tokens.insert("SPACE", vec![' ']);
 
     //Sample of a line to be processed
-    let test_string: &str = "!s s.test = \"hello\";";
+    let test_string: &str = "!s s.test, s.hola, x.bye;";
  
     
-    let mut string_set: IndexSet<char> = IndexSet::new();
+    let mut lexeme_set: IndexSet<String> = IndexSet::new();
+    let mut lexeme: String = String::new();
 
-    println!("lexemes:\n");
+    //println!("lexemes:\n");
 
     //Iterates over the string (line sample)
     for c in test_string.chars() {
-        
-        let (lexeme_end, token_type) = is_end_of_lex(c, &tokens);
-        
-        if (lexeme_end == true) && (token_type == "SEP") {
-            println!();
-        }
+        let (end_of_lexeme, token_type) = is_end_of_lex(c, &tokens);
 
+        if end_of_lexeme {
+
+            //Checks if lexeme isnt empty to avoid inserting empty string
+            if !lexeme.is_empty() {
+                lexeme_set.insert(lexeme.clone());
+                lexeme.clear();
+            }
+    
+            //Inserts separator as lexeme if its not a space
+            if token_type != "SPACE" {
+                lexeme.push(c);
+                lexeme_set.insert(lexeme.clone());
+                lexeme.clear();
+            }
+
+        }
+        
         else {
-            print!("{}", c);
+            lexeme.push(c); //Constructs the lexeme
         }
     }
 
-
+    // Inserts any leftover lexeme
+    if !lexeme.is_empty() {
+        lexeme_set.insert(lexeme);
+    }
     
-
-
-
+    print!("lexemes:\n\n{:?}\n\n", lexeme_set);
     
 }
 
